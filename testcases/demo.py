3196745c9b993052107e05d81a34ba70ded339c0
adithya <adithya.g@tibilsolutions.com>
2023-09-22 20:04:58 +0530

    Demo4

--- testcases/demo.py
+++ testcases/demo.py
@@ -1,5 +1,5 @@
-from utilities.readproperties import ReadConfig
-
-values = ReadConfig()
-result = values.read_credentials_from_csv()
-print(result[0])
+# from utilities.readproperties import ReadConfig
+#
+# values = ReadConfig()
+# result = values.read_credentials_from_csv()
+# print(result[0])
